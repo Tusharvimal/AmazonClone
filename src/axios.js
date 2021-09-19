4cffd2761a39dc8dd55d07a711dae218e72bba9e
Tusharvimal <76821257+Tusharvimal@users.noreply.github.com>
2021-09-19 22:51:29 +0530

    Update

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const instance = axios.create({
     // THE API (CLOUD FUNCTION) URL
-    baseURL : 'https://us-central1-clone-26e1c.cloudfunctions.net/api'
-    // 'http://localhost:5001/clone-26e1c/us-central1/api' --> Local Host
+    baseURL : 
+    'https://us-central1-clone-26e1c.cloudfunctions.net/api'
+    // 'http://localhost:5001/clone-26e1c/us-central1/api' 
+    // --> Local Host
 })
 
 export default instance;
